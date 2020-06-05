0d54a38a0199eb78367d20ac4cb6b92d9f2ccb47
Cord Franks <crfranks94@gmail.com>
2020-06-04 22:41:36 -0500

    added delete and add book functionality

--- app.js
+++ app.js
@@ -42,6 +42,18 @@ class UI {
 
         list.appendChild(row);
     }
+
+    static deleteBook(el) {
+        if(el.classList.contains('delete')) {
+            el.parentElement.parentElement.remove();
+        }
+    }
+
+    static clearFields() {
+        document.querySelector('#title').value = '';
+        document.querySelector("#author").value = '';
+        document.querySelector("#isbn").value = '';
+    }
 }
 
 //Store Class: Handles Storage
@@ -50,5 +62,27 @@ class UI {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 //Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    //Prevent actual submit
+    e.preventDefault();
+
+    //Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector("#author").value;
+    const isbn = document.querySelector("#isbn").value;
+
+    //Instantiate book
+    const book = new Book(title, author, isbn);
+
+    //Add book to UI
+    UI.addBookToList(book);
+
+    //Clear fields
+    UI.clearFields();
+
+});
 
 //Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    UI.deleteBook(e.target)
+})
